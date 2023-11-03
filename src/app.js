3e91705bb47a9fe9dd25a9468ecafaf12ded4dc2
Gabriel Quadros <gabrielfernand464508@gmail.com>
2023-11-03 14:52:27 -0300

    Adicionado CORS

--- src/app.js
+++ src/app.js
@@ -4,12 +4,31 @@ import { resolve } from 'path';
 dotenv.config();
 
 import './database';
+
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import imageRoutes from './routes/imageRoutes';
+
+const whiteList = [
+  'https://react.poliert.com.br',
+  'https://localhost:3000',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed by CORS'));
+    }
+  }
+}
 
 class App {
   constructor() {
@@ -19,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
